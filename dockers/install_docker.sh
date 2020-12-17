b98a819beb0f058cd7ed11c20e02ffae4cf34d82
patrick <patrickwuke@163.com>
2020-12-17 14:18:38 +0800

    update mysql scripts

--- dockers/install_docker.sh
+++ dockers/install_docker.sh
@@ -17,3 +17,10 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
+sudo apt  install docker.io
+curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+
+
+
--- dockers/mysql/import_sql.sh
+++ dockers/mysql/import_sql.sh
@@ -6,3 +6,6 @@ echo "start import ${FILE_NAME}"
 mysql -uroot -p123456 <$dir/${FILE_NAME}
 
 echo "IMPORT FILE Successfully!"
+
+# create database scheme
+mysql create database <>
