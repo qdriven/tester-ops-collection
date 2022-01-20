6c03b0bf1d8492fd7b281df26e8c96587014aafc
Patrick <patrickwuke@163.com>
2022-01-20 09:42:20 +0800

    remove unused files

--- go-tops/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-MODULE=$1
-
-cd {$MODULE}
-CGO_ENABLED=0
-## TODO build in MAC
-GOOS=linux GOARCH=amd64 vgo build -ldflags "-s -w"
-GOOS=windows GOARCH=amd64 vgo build -ldflags "-s -w"
--- go-tops/go.mod
+++
@@ -1,5 +0,0 @@
-module go-tops
-
-go 1.14
-
-require github.com/elazarl/goproxy v0.0.0-20200315184450-1f3cb6622dad
